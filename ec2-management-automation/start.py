00acafc6855dd2025ae2a1fb940a3e6d9abf8642
ahmed-hasann <168161794+ahmed-hasann@users.noreply.github.com>
2024-11-13 17:05:53 +0530

    Update start.py

--- ec2-management-automation/start.py
+++ ec2-management-automation/start.py
@@ -2,7 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client("ec2")
-    instance_id = "i-0dbbf785ffa3d2d06"
+    instance_id = "<your-instance-id>"
 
     try:
         ec2.start_instances(InstanceIds=[instance_id])
